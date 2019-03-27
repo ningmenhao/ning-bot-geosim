bc0a594e859a6dfaab794846b84327bf472391a9
Bryan Tor <bterrific2008@gmail.com>
2019-03-27 15:04:23 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const Discord = require("discord.js");
+var auth = require('./auth.json');
+const client = new Discord.Client();
+
+client.on("ready", () => {
+  console.log("I am ready!");
+});
+
+console.log(auth.token);
+
+client.on("message", (message) => {
+  
+  if (message.author.bot) return;
+    
+  if (message.content.startsWith("**Suggestion**") && message.channel.id == "454033864323629059") {
+    message.react("📌");
+    console.log(message.content+"\n");
+    
+    client.channels.get("454045860435722240").send("Suggestion from: "+message.member+"\n\n"+message.content.substring(15))
+      .then(function (message) {
+        message.react("⬆")
+        message.react("⬇")
+      }).catch(function() {
+        //Something
+      });
+       
+  }
+  
+});
+
+client.login(process.env.BOT_TOKEN);
