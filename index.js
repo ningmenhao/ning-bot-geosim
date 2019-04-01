b844a2cefda142ee9349deaf8996d32028e12730
Bryan Tor <bterrific2008@gmail.com>
2019-03-31 18:46:07 -0700

    April fools?

--- index.js
+++ index.js
@@ -23,6 +23,61 @@ client.on("message", (message) => {
        
   }
   
+  if (message.content.includes("ning")) {
+    message.react(client.emojis.get("503376027356102656"))
+    .then(reaction => console.log(typeof reaction));
+  }
+  
+  if (message.content.includes("ran")) {
+    message.react(client.emojis.get("526619819349639188"))
+    .then(reaction => console.log(typeof reaction));
+  }
+  
+  if (message.content.includes("Insert")) {
+    message.react(client.emojis.get("542566785862467584"))
+    .then(reaction => console.log(typeof reaction));
+  }
+  
+  if (message.content.includes("kai")) {
+    message.react(client.emojis.get("535933767127138332"))
+    .then(reaction => console.log(typeof reaction));
+  }
+  
+  if (message.content.includes("cav")) {
+    message.react(client.emojis.get("529094281361620992"))
+    .then(reaction => console.log(typeof reaction));
+  }
+  
+  if (message.content.includes("slime")) {
+    message.react(client.emojis.get("539631337276178432"))
+    .then(reaction => console.log(typeof reaction));
+  }
+  
+  if (message.content.includes("wren")) {
+    message.react(client.emojis.get("505513510990643201"))
+    .then(reaction => console.log(typeof reaction));
+  }
+  
+  if (message.content.includes("deusos")) {
+    message.react(client.emojis.get("513908869726994433"))
+    .then(reaction => console.log(typeof reaction));
+  }
+  
+  if (message.content.includes("erhard")) {
+    message.react(client.emojis.get("539631984931241994"))
+    .then(reaction => console.log(typeof reaction));
+  }
+  
+  if (message.content.includes("odd")) {
+    message.react(1??)
+    .then(reaction => console.log(typeof reaction));
+  }
+  
+  if (message.content.includes("hugh")) {
+    message.react(??)
+    .then(reaction => console.log(typeof reaction));
+  }
+  
 });
 
 client.login(process.env.BOT_TOKEN);
