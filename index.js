907c20e9d91de3abda886ce0f1a6b5a3df36a93b
James Townsend <j.a.townsend2000@gmail.com>
2021-02-01 10:11:47 +0000

    Season XV :D

--- index.js
+++ index.js
@@ -197,9 +197,9 @@ client.on("message", message => {
   }
 
   if (message.content === "!date") {
-    let seasonStart = Date.UTC(2020, 9, 4);
+    let season0thSunday = Date.UTC(2021, 0, 31); // Remember months count from 0
     let now = Date.now();
-    let year = (Math.floor((now - seasonStart)/604800000) + 2020).toString();
+    let year = (Math.floor((now - season0thSunday)/604800000) + 2020).toString();
     let lookup = ["January", "February/March", "April/May", "June", "July/August", "September/October", "November/December"];
     let month = lookup[new Date().getDay()];
     message.channel.send("The date is " + month + " " + year + ".");
