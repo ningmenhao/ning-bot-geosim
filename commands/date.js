986665c867377d5eabe23428c26bc7df640cf1b2
KrennicSloane <88780687+KrennicSloane@users.noreply.github.com>
2021-08-11 12:39:50 -0400

    Update date.js

--- commands/date.js
+++ commands/date.js
@@ -7,7 +7,7 @@ module.exports = {
 
 		const now = Date.now();
 		const year = (Math.floor((now - season0thSunday) / 604800000) + 2021).toString();
-		const lookup = ['January', 'February/March', 'April/May', 'June', 'July/August', 'September/October', 'November/December'];
+		const lookup = ['November/December', 'January', 'February/March', 'April/May', 'June', 'July/August', 'September/October'];
 		const month = lookup[new Date().getDay()];
 
 		await interaction.reply(`The date is ${month} ${year}!`);
