ca127f025161a1bfaaf1eac93b5e2852ef37b044
ningmenhao <purelymadgenius@gmail.com>
2022-01-06 20:37:33 +0000

    Change date command to use season starting date

--- commands/date.js
+++ commands/date.js
@@ -3,10 +3,10 @@ module.exports = {
 	description: 'Replies with the current ingame date!',
 	async execute(interaction) {
 		// Remember months count from 0. January is 0, August is 7, December is 11
-		const season0thSunday = Date.UTC(2021, 10, 7);
+		const seasonStartDate = Date.UTC(2021, 10, 8);
 
 		const now = Date.now();
-		const year = (Math.floor((now - season0thSunday) / 604800000) + 2021).toString();
+		const year = (Math.floor((now - seasonStartDate) / 604800000) + 2021).toString();
 		const lookup = ['November/December', 'January', 'February/March', 'April/May', 'June', 'July/August', 'September/October'];
 		const month = lookup[new Date().getDay()];
 
