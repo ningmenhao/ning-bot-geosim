0eaf2d3a5fe606e0e0c248a745ab01902dd63c13
ningmenhao <purelymadgenius@gmail.com>
2022-06-30 16:05:43 -0700

    Correct incorrect import in guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,9 +1,9 @@
-const fs = require('fs');
+const { MessageEmbed } = require('discord.js');
 module.exports = {
 	name: 'guildMemberAdd',
 	execute(member){
 		console.log(`User ${member.user.id} joined the server`);
-		let welcomembed = new Discord.MessageEmbed()
+		let welcomembed = new MessageEmbed()
         		.setAuthor(`${member.user.tag} just joined!`, member.user.avatarURL())
         		.setDescription("Welcome to Angry Birds Economy Server! Don't forget to read the <#748333038294794241>! <:WelcomePigHappy:777683105863041054>")
         		.setColor("FF0000");
