208ae22a298566df0dc5808a38f3459d4918725d
ningmenhao <purelymadgenius@gmail.com>
2022-06-30 16:17:44 -0700

    Refine welcome message as an embed

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -5,7 +5,7 @@ module.exports = {
 		console.log(`User ${member.user.id} joined the server`);
 		let welcomeEmbed = new MessageEmbed()
         		.setAuthor(`${member.user.tag} just joined!`, member.user.avatarURL())
-        		.setDescription("Welcome to Angry Birds Economy Server! Don't forget to read the <#748333038294794241>! <:WelcomePigHappy:777683105863041054>")
+        		.setDescription(`<@${member.user.id}>, welcome to Geosim! A mod will be in shortly to give you access to the rest of the server. In the meantime, let us know where you found us! <@&214543567878750208>`)
         		.setColor("FF0000");
     		member.guild.channels.cache.get("712949115943911475").send({ embeds: [welcomeEmbed] })
 			.catch((err) => console.log(err));
