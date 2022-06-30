25414bbc0f64a4a19b41aae5a2a80b9baf022e5b
ningmenhao <purelymadgenius@gmail.com>
2022-06-30 15:51:05 -0700

    Add logging for guildMemberAdd event

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 module.exports = {
 	name: 'guildMemberAdd',
 	execute(member){
+		console.log(`User ${guildMember.user.id} joined the server`);
 		guildMember.guild.channels.cache.get('712949115943911475').send(
 			`**@${guildMember.user.id}, welcome to Geosim!** A mod will be in shortly to give you access to the rest of the server. In the meantime, let us know where you found us! @Moderator`
 		);
