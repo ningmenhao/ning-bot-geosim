4ff1408b39e6c133c593184912dd24943c5de6a7
ningmenhao <purelymadgenius@gmail.com>
2022-06-30 15:54:37 -0700

    Correct typo in guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -2,9 +2,9 @@ const fs = require('fs');
 module.exports = {
 	name: 'guildMemberAdd',
 	execute(member){
-		console.log(`User ${guildMember.user.id} joined the server`);
-		guildMember.guild.channels.cache.get('712949115943911475').send(
-			`**@${guildMember.user.id}, welcome to Geosim!** A mod will be in shortly to give you access to the rest of the server. In the meantime, let us know where you found us! @Moderator`
+		console.log(`User ${member.user.id} joined the server`);
+		member.guild.channels.cache.get('712949115943911475').send(
+			`**@${member.user.id}, welcome to Geosim!** A mod will be in shortly to give you access to the rest of the server. In the meantime, let us know where you found us! @Moderator`
 		);
 	},
 };
