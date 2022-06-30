b72ff605426716fb38052d2bb24c89299cdeb0d0
ningmenhao <purelymadgenius@gmail.com>
2022-06-30 16:11:44 -0700

    Fix message embed

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -3,11 +3,11 @@ module.exports = {
 	name: 'guildMemberAdd',
 	execute(member){
 		console.log(`User ${member.user.id} joined the server`);
-		let welcomembed = new MessageEmbed()
+		let welcomeEmbed = new MessageEmbed()
         		.setAuthor(`${member.user.tag} just joined!`, member.user.avatarURL())
         		.setDescription("Welcome to Angry Birds Economy Server! Don't forget to read the <#748333038294794241>! <:WelcomePigHappy:777683105863041054>")
         		.setColor("FF0000");
-    		member.guild.channels.cache.get("712949115943911475").send(welcomembed)
+    		member.guild.channels.cache.get("712949115943911475").send({ embeds: [welcomeEmbed] })
 			.catch((err) => console.log(err));
 	},
 };
