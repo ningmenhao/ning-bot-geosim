adaf84f5cfb6595224042797d67e86b344ce8bf4
Bryan Tor <bterrific2008@gmail.com>
2022-07-23 00:59:26 -0700

    Add missing https to link for players command

--- commands/players.js
+++ commands/players.js
@@ -8,7 +8,7 @@ module.exports = {
 			.setColor('#5b92e5')
 			.setTitle('List of Current and Open Claims')
 			.setURL('https://www.reddit.com/r/geosim/wiki/players')
-			.setDescription("[Check the player master list](https://www.reddit.com/r/geosim/wiki/players) or [our website](geosim.xpowers.cyou)");
+			.setDescription("[Check the player master list](https://www.reddit.com/r/geosim/wiki/players) or [our website](https://geosim.xpowers.cyou)");
 
 		await interaction.reply({embeds: [playerlistEmbed]});
 	},
